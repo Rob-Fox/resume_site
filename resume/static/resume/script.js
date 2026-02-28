f0cc029b2b7400588270f80c8ce6e1ac7d286c7d
Robert Fox <robert.fox@metergroup.com>
2026-02-27 21:13:40 -0800

    decided to make cards for jobs, added possible svgs for card layout but instead trying pure html/css, taking notes on card measurements in script

--- resume/static/resume/script.js
+++ resume/static/resume/script.js
@@ -1,3 +1,22 @@
+// card measurements in mm
+card_width = 63;
+card_height = 88;
+outer_border_width = 2;
+bottom_border_height = 6;
+bottom_border_inner_height = 1;
+inner_border_width = 2;
+inner_band = 1;
+top_field_height = 5;
+type_field_height = 5;
+// outer border has rounded outside and inside corners, outside over 2 mil and inside over .5 mil
+// all side borders on name box 1 mil with .4 mil as top highlight
+// side borders on picture frame 1 mil
+// all side borders on creature type 1 mil
+// side and bottom border on text box 1 mil
+// .5 mil border on power/toughness with .5 mil drop shadow (down and left)
+
+
+
 document.addEventListener('DOMContentLoaded', function(){
     linkedin_svg_element = document.getElementById('linkedin_svg');
     linkedin_color = '#0A66C2';
@@ -9,7 +28,6 @@ document.addEventListener('DOMContentLoaded', function(){
     github_color = '#000000';
     github_svg_element.setAttribute('fill', github_color);
     github_path_element = document.getElementById('github_path');
-    // github_path = 'M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.6.111.816-.258.816-.575 0-.285-.011-1.04-.016-2.04-3.338.724-4.043-1.61-4.043-1.61-.542-1.375-1.328-1.748-1.328-1.748-1.089-.744.083-.729.083-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.462-2.381 1.236-3.221-.124-.3-.533-1.527.117-3.175 0 0 1-.322 3.3 1.23.957-.266 1.983-.4 3-.4.011 0 .021 0 .032 0 1.016 0 2.042.134 3 .4 2.292-1.552 3.292-1.23 3.292-1.23.65 1.648.237 2.875.118 3.175.774.84 1.235 1.911 1.235 3.221 0 4.61-2.801 5.624-5.475 5.921.42.359.801 1.07.801 2.15l-.011 3.926c0 .319.208.682.822.575C20.565 21.8 24 17.302 24 12c0-6.627-5.373-12-12-12z'
     github_path = 'M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.026 2.747-1.026.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z';
     github_path_element.setAttribute('d', github_path);
 })
