3e7d5074ff56845f5b525e3ea65c268793939184
Robert Fox <robert.fox@metergroup.com>
2026-03-03 20:10:37 -0800

    modified css/js to only display first job card (and only one at a time) until arrow is clicked, started creating JS vars for programmatic css measurements

--- resume/static/resume/script.js
+++ resume/static/resume/script.js
@@ -1,13 +1,13 @@
 // card measurements in mm
-card_width = 63;
-card_height = 88;
-outer_border_width = 2;
-bottom_border_height = 6;
-bottom_border_inner_height = 1;
-inner_border_width = 2;
-inner_band = 1;
-top_field_height = 5;
-type_field_height = 5;
+// card_width = 63;
+// card_height = 88;
+// outer_border_width = 2;
+// bottom_border_height = 6;
+// bottom_border_inner_height = 1;
+// inner_border_width = 2;
+// inner_band = 1;
+// top_field_height = 5;
+// type_field_height = 5;
 // outer border has rounded outside and inside corners, outside over 2 mil and inside over .5 mil
 // all side borders on name box 1 mil with .4 mil as top/right highlight
 // side borders on picture frame 1 mil
@@ -15,9 +15,52 @@ type_field_height = 5;
 // side and bottom border on text box 1 mil
 // .5 mil border on power/toughness with .5 mil drop shadow (down and left)
 
+const card_width = 63;
+const card_height = 88;
+const outer_card_border = 3;
+const inner_card_width = 59;
+const inner_card_height = 76;
+const inner_card_margin = 2;
+const inner_card_border = 2;
+const inner_card_padding = 1;
+const inner_card_border_radius = [2, 2, 5, 5];
+
+const text_bar_width = 55;
+const text_bar_height = 55;
+const text_bar_border_radius = 55;
+const text_bar_box_shadow = [[0, 0, 0, .5], [1, -1, .25, -.75], [-1, 1, .25, -.75]];
+const text_bar_font_size = .80; //percentage
+const text_bar_padding_left = .02; //percentage
+const text_bar_line_height = 5;
+
+const image_box_width = 53;
+const image_box_height = 39;
+const image_box_margin_top_bottom = .6;
+const image_box_box_shadow = [0, 0, 0, .5];
+
+const text_box_width = 53;
+const text_box_height = 26;
+const text_box_margin_top = .6;
+const text_box_box_shadow = [0, 0, 0, .5];
+const text_box_font_size = .5; //precentage
 
 
 document.addEventListener('DOMContentLoaded', function(){
+    draw_socials();
+
+    let [card_container, children, next, previous] = initialize_cards();
+    let current_idx = 0;
+    next.addEventListener('click', () => {
+        current_idx = (current_idx + 1) % children.length;
+        switch_child(current_idx, children);
+    })
+    previous.addEventListener('click', () => {
+        current_idx = (current_idx - 1 + children.length) % children.length;
+        switch_child(current_idx, children);
+    })
+})
+
+function draw_socials(){
     linkedin_svg_element = document.getElementById('linkedin_svg');
     linkedin_color = '#0A66C2';
     linkedin_svg_element.setAttribute('fill', linkedin_color);
@@ -30,4 +73,18 @@ document.addEventListener('DOMContentLoaded', function(){
     github_path_element = document.getElementById('github_path');
     github_path = 'M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.026 2.747-1.026.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z';
     github_path_element.setAttribute('d', github_path);
-})
+}
+
+function switch_child(idx, children){
+    children.forEach((child, i) => {
+        child.style.display = i === idx ? 'block' : 'none';
+    });
+}
+
+function initialize_cards(){
+    let card_container = document.getElementById('job_div');
+    let children = card_container.querySelectorAll('.card_outer')
+    let next_button = document.getElementById('next');
+    let previous_button = document.getElementById('previous');
+    return [card_container, children, next_button, previous_button];
+}
